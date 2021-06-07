5b121a46e4acd92f750330df85f12ca0bcf6d3fc
Edd <fausten17otmail.com>
2021-06-07 16:03:23 -0500

    history

--- src/sc.js
+++ src/sc.js
@@ -1,48 +1,27 @@
-import React, { useState } from "react";
 import Square from "./fc";
 
 const Booardo = (props) => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState("X");
-
-  const handleClick = (i) => {
-    const newarr = [...board];
-    if (props.winner(newarr) || newarr[i]) {
-      return;
-    }
-    newarr[i] = turn;
-    setTurn(turn === "X" ? "O" : "X");
-    setBoard(newarr);
-    props.addToHistory(board);
-    /* cuando el estado de Board cambia, los componentes Square se re-renderiza automáticamente. */
-  };
-
-  let status;
-  let winner = props.winner(board);
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = "Next turn: " + turn;
-  }
-
   return (
     <div>
-      <div className="status">{status}</div>
+      <div className="status">{props.status}</div>
       <div className="board-row">
         {/* En el onClick hay que pasar () => y por que si no handleClick provoca un infinite loop for render*/}
-        <Square value={board[0]} onClick={() => handleClick(0)} />
-        <Square value={board[1]} onClick={() => handleClick(1)} />
-        <Square value={board[2]} onClick={() => handleClick(2)} />
+        {/* A different callback is created each time the Square component render */}
+        {/* The () function creates a new bound function, which is an exotic function object that wraps the original function 
+        object. Calling the bound function generally results in the execution of its wrapped function. */}
+        <Square value={props.board[0]} onClick={() => props.handleClick(0)} />
+        <Square value={props.board[1]} onClick={() => props.handleClick(1)} />
+        <Square value={props.board[2]} onClick={() => props.handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={board[3]} onClick={() => handleClick(3)} />
-        <Square value={board[4]} onClick={() => handleClick(4)} />
-        <Square value={board[5]} onClick={() => handleClick(5)} />
+        <Square value={props.board[3]} onClick={() => props.handleClick(3)} />
+        <Square value={props.board[4]} onClick={() => props.handleClick(4)} />
+        <Square value={props.board[5]} onClick={() => props.handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={board[6]} onClick={() => handleClick(6)} />
-        <Square value={board[7]} onClick={() => handleClick(7)} />
-        <Square value={board[8]} onClick={() => handleClick(8)} />
+        <Square value={props.board[6]} onClick={() => props.handleClick(6)} />
+        <Square value={props.board[7]} onClick={() => props.handleClick(7)} />
+        <Square value={props.board[8]} onClick={() => props.handleClick(8)} />
       </div>
     </div>
   );
--- src/tc.js
+++ src/tc.js
@@ -3,6 +3,8 @@ import Board from "./sc";
 
 const Game = (props) => {
   const [history, setHistory] = useState([]);
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState("X");
 
   const addToHistory = (array) => {
     const newEntry = [...history];
@@ -28,25 +30,52 @@ const Game = (props) => {
       }
     }
     return null;
+    /* If you want a component to hide itself even though it was rendered by another component. To do this return null 
+    instead of its render output */
+  };
+
+  const handleClick = (i) => {
+    const newarr = [...board];
+    if (checkWinner(newarr) || newarr[i]) {
+      return;
+    }
+    newarr[i] = turn;
+    setTurn(turn === "X" ? "O" : "X");
+    setBoard(newarr);
+    addToHistory(board);
+    /* cuando el estado de Board cambia, los componentes Square se re-renderiza automáticamente. */
+  };
+
+  let status;
+  let winner = checkWinner(board);
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next turn: " + turn;
+  }
+
+  const ListItem = (props) => {
+    return (
+      <li>
+        <button onClick={() => setBoard(props.board)}>
+          Go to move: {props.value}
+        </button>
+      </li>
+    );
   };
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board winner={checkWinner} addToHistory={addToHistory} />
+        <Board handleClick={handleClick} board={board} status={status} />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
-        {history.map((move) => {
-          let movement = history.indexOf(move);
-          return (
-            <ol>
-              <button onClick={() => console.log(history[movement])}>
-                Go to move: {movement}
-              </button>
-            </ol>
-          );
-        })}
+        <ul>
+          {history.map((move, index) => (
+            /* elements inside the map() call need keys. */
+            <ListItem key={index} value={index} board={move} />
+          ))}
+        </ul>
       </div>
     </div>
   );
